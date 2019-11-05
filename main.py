ab516df13f9c26854efe681d98125883c099400d
Luis Ferreira <luis.ferreira@dextra-sw.com>
2019-11-05 16:01:15 -0300

    Primeira versão do Crud de Video

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from views.categoria import categorias_bp
+from views.video import videos_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(videos_bp)
 app.register_blueprint(categorias_bp)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/dextraflix"
 mongo = PyMongo(app)
-
-
-
